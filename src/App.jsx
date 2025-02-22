f8fb1f47b06f07affd8b1ae695c2f5b9b8c96d08
bernakocalar <berna.kocalar@gmail.com>
2025-02-22 22:19:34 +0300

    app.jsx styled components ile düzenlendi ve app.css silindi

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 80px;
+  text-align: center;
+  color: white;
+`;
+
+const Title = styled.h1`
+  margin-top: 10px;
+  font-size: 1.4rem;
+`;
+
+const Slogan = styled.p`
+  margin-top: 10px;
+  font-size: 3.5rem;
+`;
+
+const AppWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/iteration-1-images/home-banner.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const FirstButton = styled.button`
+  background-color: #FDC913;
+  margin-top: 10px;
+  border-radius: 20px;
+`;
+
 function App() {
-  const navigate = useNavigate(); 
-  
+  const navigate = useNavigate();
+
   return (
-    <>
-    <div className="app">
-    <div id="container">
-      <h1 id="title">Teknolojik Yemekler</h1>
-      <p id="slogan">KOD ACIKTIRIR. PİZZA, DOYURUR.</p>
-      <button 
-        onClick={() => navigate('/orderPage')} 
-        id="firstButton">
-        ACIKTIM
-      </button>
-    </div>
-    </div>
-      </>
+    <AppWrapper>
+      <Container>
+        <Title>Teknolojik Yemekler</Title>
+        <Slogan>KOD ACIKTIRIR. PİZZA, DOYURUR.</Slogan>
+        <FirstButton onClick={() => navigate('/orderPage')} id="firstButton">
+          ACIKTIM
+        </FirstButton>
+      </Container>
+    </AppWrapper>
   );
 }
+
 export default App;
