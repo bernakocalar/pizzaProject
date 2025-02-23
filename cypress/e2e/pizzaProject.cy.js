c77e86cccd7e5750beb422c79be4725e3d41f75e
bernakocalar <berna.kocalar@gmail.com>
2025-02-23 21:03:53 +0300

    test güncellemeleri

--- cypress/e2e/pizzaProject.cy.js
+++ cypress/e2e/pizzaProject.cy.js
@@ -1,7 +1,7 @@
 describe('Pizza Order Page', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:5178/orderPage'); // Eğer baseUrl yoksa buraya tam URL'yi koy!
+    cy.visit("http://localhost:5179/orderPage"); // Eğer baseUrl yoksa buraya tam URL'yi koy!
   });
 
   it('should input a name correctly', () => {
@@ -13,16 +13,16 @@ describe('Pizza Order Page', () => {
 
   it('should allow multiple toppings to be selected', () => {
     cy.get('input[type="checkbox"][value="pepperoni"]').should('be.visible').check();
-    cy.get('input[type="checkbox"][value="chicken"]').check();
-    cy.get('input[type="checkbox"][value="corn"]').check();
-    cy.get('input[type="checkbox"][value="garlic"]').check();
-    cy.get('input[type="checkbox"][value="pineapple"]').check();
+    cy.get('input[type="checkbox"][value="tavuk ızgara"]').check();
+    cy.get('input[type="checkbox"][value="mısır"]').check();
+    cy.get('input[type="checkbox"][value="sarımsak"]').check();
+    cy.get('input[type="checkbox"][value="ananas"]').check();
     cy.get('input[type="checkbox"]:checked').should('have.length', 5);
   });
 
   it('should submit the order form', () => {
-    cy.get('input[name="size"][value="medium"]').check();
-    cy.get('select[name="dough"]').select('thin');
+    cy.get('input[name="size"][value="orta"]').check();
+    cy.get('select[name="dough"]').select('ince');
     cy.get('input[type="checkbox"][value="pepperoni"]').check();
     cy.get('textarea[name="name"]').type('John Doe');
     cy.get('textarea[name="note"]').type('No onions, please.');
