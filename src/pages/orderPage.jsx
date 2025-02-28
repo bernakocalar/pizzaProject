f4fc11be2202c69f15ec3a21ddd2c0fdc7970cad
bernakocalar <berna.kocalar@gmail.com>
2025-02-28 16:46:16 +0300

    css dosyası iimportu silindi

--- src/pages/orderPage.jsx
+++ src/pages/orderPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './orderPage.css';
 import OrderSuccess from "./orderSuccess";
 import { styled } from 'styled-components';
 
